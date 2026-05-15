0baa9e08fc3d61b7d922e17c013f77b8114df051
brauliojpp26-beep <braulio.jpp26@gmail.com>
2026-05-15 17:05:42 -0600

    Initial commit

---
+++ src/main/java/com/jonathan/sales_management_api/SalesManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.sales_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagementApiApplication.class, args);
+	}
+
+}
